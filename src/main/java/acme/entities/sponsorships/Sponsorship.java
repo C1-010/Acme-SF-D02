a62a22b65a0e7e315a31a97ecfe7175e2e6acf1b
pabdiaord <pabdiaord@alum.us.es>
2024-02-20 20:36:39 +0100

    D02-R02-Sponsorship entity created v1

---
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -0,0 +1,60 @@
+
+package acme.entities.sponsorships;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Sponsorship extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				moment;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@PastOrPresent
+	@NotNull
+	private Date				duration;
+
+	@Min(1)
+	private Double				amount;
+
+	@NotNull
+	private SponsorshipType		type;
+
+	private String				email;
+
+	@URL
+	private String				link;
+
+}
