3295b224730fc9a736ff016bc1acbf0d18cc8192
javnievic <javiernieto03.jnv@gmail.com>
2024-02-20 19:17:26 +0100

    D02-Contract entity created v1

---
+++ src/main/java/acme/entities/contracts/Contract.java
@@ -0,0 +1,52 @@
+
+package acme.entities.contracts;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Contract extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				providerName;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				goals;
+
+	private Double				budget;
+
+}
