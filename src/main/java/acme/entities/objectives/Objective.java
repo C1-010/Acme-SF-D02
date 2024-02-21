d71dbba40c4c45a8ac767439c6c3e817b965eb47
javnievic <javiernieto03.jnv@gmail.com>
2024-02-21 01:10:58 +0100

    D02-Objective entity created v1

---
+++ src/main/java/acme/entities/objectives/Objective.java
@@ -0,0 +1,57 @@
+
+package acme.entities.objectives;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Objective extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	private Priority			priority;
+
+	private boolean				status;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				duration;
+
+	@Length(max = 255)
+	private String				optionalLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+}
