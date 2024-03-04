5536be3510ad041386db92a4ca9d5f439ee8d1b4
pabdiaord <pabdiaord@alum.us.es>
2024-03-04 20:15:24 +0100

    D02-R02-Sponsorship entity little change

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.URL;
 
@@ -46,13 +47,10 @@ public class Sponsorship extends AbstractEntity {
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	private Date				startDuration;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	private Date				endDuration;
+	private Date				duration;
 
 	@NotNull
+	@Positive
 	private Money				amount;
 
 	@NotNull
