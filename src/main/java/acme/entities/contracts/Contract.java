25ab3ffe8a6f03f6eef393313440781d1357a90c
javnievic <javiernieto03.jnv@gmail.com>
2024-03-06 18:20:31 +0100

    D02-S2-REVIEW-code review changes v1

--- src/main/java/acme/entities/contracts/Contract.java
+++ src/main/java/acme/entities/contracts/Contract.java
@@ -11,7 +11,7 @@ import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
@@ -19,6 +19,7 @@ import org.hibernate.validator.constraints.Length;
 import acme.client.data.AbstractEntity;
 import acme.client.data.datatypes.Money;
 import acme.entities.projects.Project;
+import acme.roles.Client;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -35,11 +36,11 @@ public class Contract extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}", message = "{validation.contract.code}")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$", message = "{validation.contract.code}")
 	private String				code;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Past
+	@PastOrPresent
 	@NotNull
 	private Date				instantiationMoment;
 
@@ -56,6 +57,7 @@ public class Contract extends AbstractEntity {
 	private String				goals;
 
 	//TODO The budget must be less than or equal to the corresponding project cost
+	@NotNull
 	private Money				budget;
 
 	// Derived attributes -----------------------------------------------------
@@ -66,5 +68,10 @@ public class Contract extends AbstractEntity {
 	@Valid
 	@ManyToOne(optional = false)
 	private Project				project;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Client				client;
 
 }
