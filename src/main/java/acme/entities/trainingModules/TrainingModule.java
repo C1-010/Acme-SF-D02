cefa2f6fcbdf32b0a0c71e685a22c85d06db2084
Antonio José López Cubiles <antlopcub@alum.us.es>
2024-03-06 19:03:33 +0100

    Student 2 Fixes + Developer Role

--- src/main/java/acme/entities/trainingModules/TrainingModule.java
+++ src/main/java/acme/entities/trainingModules/TrainingModule.java
@@ -5,16 +5,20 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,30 +35,37 @@ public class TrainingModule extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$", message = "{validation.trainingmodule.code}")
 	private String						code;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Past
+	@PastOrPresent
 	private Date						creationMoment;
 
 	@NotBlank
 	@Length(max = 100)
 	private String						details;
 
+	@NotNull
 	private TrainingModuleDifficulty	difficultyLevel;
 
-	@Past
+	@PastOrPresent
 	//antes del creationMoment
 	private Date						updateMoment;
 
 	@URL
 	private String						optionalLink;
 
+	@NotNull
 	private Integer						totalTime; //Para ponerlo en horas
 
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project						project;
 
 }
